ff0bfc4d2b724c00d812f4fd06c203b3e956a08b
Jason Chaves Schmidt <jchaves@ciasc.sc.gov.br>
2024-09-14 06:31:14 -0300

    Add files via upload

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  // Esta é a minha API_KEY, para fazer os testes crie a sua chave na página:
+  // https://aistudio.google.com/app/prompts/new_chat?pli=1
+  googleAiApiKey: 'AIzaSyDzsy56W4hCPuDCN6sbrex8aJ4HF7FJkLI',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-
+export const environment = {
+  //TODO: Usando API_KEY de desenvolvimento aqui
+  /**
+   * Não tem ainda API_KEY do Google Gemini de produção
+   */
+  googleAiApiKey: 'AIzaSyDzsy56W4hCPuDCN6sbrex8aJ4HF7FJkLI' };
